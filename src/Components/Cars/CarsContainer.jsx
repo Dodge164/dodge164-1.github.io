39ccc0c47f97644b327b452d204820809c4a2de2
Dodge164 <dodge164@mail.ru>
2021-09-07 22:51:46 +0400

    added pictures

--- src/Components/Cars/CarsContainer.jsx
+++ src/Components/Cars/CarsContainer.jsx
@@ -1,29 +1,24 @@
+/* eslint-disable operator-linebreak */
 /* eslint-disable no-unused-vars */
 import React, { useContext, useEffect, useState } from 'react';
 import s from './Cars.module.scss';
 
-import { getCarList, url } from '../../Api/http';
+import { getCarList } from '../../Api/http';
 // import Context from '../../context';
 
 export default function CarsContainer() {
-  // const { orderInfo } = useContext(Context);
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
   const [carList, setCarList] = useState([]);
   useEffect(async () => {
     const listOfCars = await getCarList();
     setCarList(listOfCars);
-    // console.log('listOfCars', listOfCars);
-    // получаем массив с авто
   }, []);
+
   console.log('carlist', carList);
-  //   useEffect(async () => {
-  //     if (orderInfo.name !== null) {
-  //       const carId = carList.find((item) => item.name === orderInfo.name);
-  //       console.log('model', carId);
-  //     }
-  //   }, [orderInfo.name]);
 
   return (
     <>
+      {console.log('URL', BASE_URL)}
       <div>cars</div>
       <table className="table table-hover">
         <thead>
@@ -31,21 +26,35 @@ export default function CarsContainer() {
             <th scope="col">Класс</th>
             <th scope="col">Модель</th>
             <th scope="col">Гос. номер</th>
-            {/* <th scope="col">Фото</th> */}
+            <th scope="col">Фото</th>
           </tr>
         </thead>
         <tbody>
-          {carList?.map((car) => (
-            <tr key={car?.id}>
-              <td>{car?.categoryId?.name}</td>
-              <td>{car?.name}</td>
-              <td>{car?.number}</td>
-              <td>
-                <img className={s.photo} alt="car" src={car?.thumbnail?.path} />
-              </td>
-              {/* <td>{`${url}${car?.thumbnail?.path}`}</td> */}
-            </tr>
-          ))}
+          {carList.length > 0 &&
+            carList?.map((car) => (
+              <tr key={car?.id}>
+                <td>{car?.categoryId?.name}</td>
+                <td>{car?.name}</td>
+                <td>{car?.number}</td>
+                <td>
+                  <img
+                    className={s.photo}
+                    alt="car"
+                    src={
+                      car?.thumbnail?.path.includes('base64')
+                        ? car?.thumbnail?.path
+                        : BASE_URL + car?.thumbnail?.path
+                    }
+                  />
+                  {/* {console.log(
+                    'THN',
+                    car?.thumbnail?.path.includes('base64')
+                      ? ''
+                      : `${BASE_URL}${car?.thumbnail?.path}`,
+                  )} */}
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </>
